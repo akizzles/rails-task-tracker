841310e9cf3018e473aee5eeb81e62dccd0c0a01
akizzles <aykim002@gmail.com>
2017-02-18 17:59:03 -0500

    added more user validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205210955) do
+ActiveRecord::Schema.define(version: 20170218225205) do
 
   create_table "items", force: :cascade do |t|
     t.string   "description"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170205210955) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
